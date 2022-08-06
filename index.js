c616eb546046ef749f75592c13f92108ac855545
moeini-dev <moeini.dev@gmail.com>
2022-08-06 10:27:24 +0330

    Change lisetn logic to use sequelize to sync the database and app

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./api/users/user.router');
+const db = require('./models')
+const PORT = process.env.APP_PORT || 5000
 
-app.use(express.json()); 
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use('/api/users', userRouter);
 
-app.listen(process.env.APP_PORT, () => console.log(`Server running on port ${process.env.APP_PORT} ...`));
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+})
+
+// Without using Sequelize : 
+//app.listen(process.env.APP_PORT, () => console.log(`Server running on port ${process.env.APP_PORT} ...`));
 
 
