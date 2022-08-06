98aa41828f869fdd4984e8fe910ada706ee2c072
moeini-dev <moeini.dev@gmail.com>
2022-08-06 10:19:19 +0330

    Delete just a comment

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -6,7 +6,6 @@ module.exports = {
         let token = req.get('authorization');
         if(token) {
             token = token.slice(7);
-            // 'qwe1234' is the secret key that should not be hard coded! Using .env file and environment variables are recommended
             const secretKey = process.env.SC_KEY;
             verify(token, secretKey, (err, decoded) => {
                 if(err) {
